43fb07464393dbae36c984abe8f08c73f50a653c
ENL <evgenialisunova520@gmail.com>
2024-06-24 18:40:27 +0400

    hwSem10

---
+++ hwSem10/data.js
@@ -0,0 +1,93 @@
+const albomsEl = `
+[
+    {
+      "userId": 1,
+      "id": 1,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "quidem molestiae enim"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "sunt qui excepturi placeat culpa"
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "omnis laborum odio"
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "non esse culpa molestiae omnis sed optio"
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "eaque aut omnis a"
+    },
+    {
+      "userId": 1,
+      "id": 6,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "natus impedit quibusdam illo est"
+    },
+    {
+      "userId": 1,
+      "id": 7,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "quibusdam autem aliquid et et quia"
+    },
+    {
+      "userId": 1,
+      "id": 8,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "qui fuga est a eum"
+    },
+    {
+      "userId": 1,
+      "id": 9,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "saepe unde necessitatibus rem"
+    },
+    {
+      "userId": 1,
+      "id": 10,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "distinctio laborum qui"
+    },
+    {
+      "userId": 2,
+      "id": 11,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "quam nostrum impedit mollitia quod et dolor"
+    },
+    {
+      "userId": 2,
+      "id": 12,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "consequatur autem doloribus natus consectetur"
+    },
+    {
+      "userId": 2,
+      "id": 13,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "ab rerum non rerum consequatur ut ea unde"
+    },
+    {
+      "userId": 2,
+      "id": 14,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "ducimus molestias eos animi atque nihil"
+    },
+    {
+      "userId": 2,
+      "id": 15,
+      "link": "https://img.freepik.com/free-photo/beautiful-rose-studio_23-2150894547.jpg?t=st=1719239404~exp=1719243004~hmac=bcff5ef561985d78297857982998fc5eaf0618594cc850f56e23475cad3f17c0&w=740",
+      "title": "ut pariatur rerum ipsum natus repellendus praesentium"
+    }
+  ]`
