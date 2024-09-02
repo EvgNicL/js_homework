b05f1997d2da7873cedca8d665153a3a8e2073ef
ENL <evgenialisunova520@gmail.com>
2024-09-02 13:26:32 +0400

    hw3 part6

---
+++ part6_browsersAPI/hw3_Unsplash/js/fns.js
@@ -0,0 +1,108 @@
+"use strict";
+        
+export const main = document.querySelector(".main");
+const currentDate = new Date();
+let startNumberDate = 244;    
+  
+// Вычислить номер текущего дня
+function numberOfCurrentDate (date) {
+    const num = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+    return num;
+};
+
+// Получить индекс изображения на текущий день
+function getIndexPhoto () {
+    const currentNum = numberOfCurrentDate(currentDate);
+    if (currentNum === 365) startNumberDate = 1;
+    return currentNum - startNumberDate;
+};
+
+// Загрузка данных с Unsplash
+async function fetchPhotos() {
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/photos?per_page=8&client_id=-xPKtySGMmki6q2HJfpw9SRuzq5ZNyqwSGcwpaXNH_4`
+      );
+      const photos = await response.json();
+      return photos;
+    } catch (error) {
+      console.log("Ошибка при загрузке фотографий: ", error);
+      return [];
+    };
+};
+
+// Загрузка данных в HTML
+function insertHTML(photo) {
+    main.insertAdjacentHTML(
+      "afterbegin",
+      ` <div>
+          <div class="container-img">
+            <img class="img" src="${
+              photo.urls.small
+            }" alt="photo">
+            <h4>${photo.alt_description}</h4>
+            <p>${photo.created_at.slice(0, 7)}</p>
+          </div>
+          <div class="likes-box">
+            <img src="/part6_browsersAPI/hw3_Unsplash/finger_top_icon.png" class="icon click" alt="icon" width="35px">
+            <p class="count-likes">Количество лайков: ${photo.likes}</p>
+          </div>
+        </div>
+    `);
+};
+
+// Загрузка фото дня по порядку
+export async function loadNextPhoto() {
+    const index = getIndexPhoto();
+    const photos = await fetchPhotos();
+    if (photos.length > 0) {
+        index = index % photos.length;
+        insertHTML(photos[index]);
+        addToHistory(photos[index]);
+    };
+    return photos;
+};
+
+// Загрузка рандомного фото дня
+export async function loadRandomPhoto() {
+    const photos = await fetchPhotos();
+    if (photos.length > 0) {
+        if (startNumberDate < numberOfCurrentDate(currentDate)) {
+            const index = Math.floor(Math.random() * photos.length);
+            insertHTML(photos[index]);
+            addToHistory(photos[index]);
+        } else  insertHTML(photos[0]);
+    };
+    startNumberDate = numberOfCurrentDate(currentDate);
+    return photos;
+
+};
+
+// Записать данные в историю
+function addToHistory (photo) {
+    let history = getHistory();
+    history.push({
+    url: photo.urls.small,
+    likes: photo.likes
+  });
+  localStorage.setItem("photoHistory", JSON.stringify(history));
+};
+
+// Изменить количество лайков в истории
+export function changeHistory (url, likes) {                  
+    let history = getHistory();
+    history.forEach(element => {
+      if (element.url === url) {
+        element.likes = likes;
+      }
+      localStorage.setItem("photoHistory", JSON.stringify(history));
+    });
+};
+
+// Получить историю из localStorage
+function getHistory () {
+    if (localStorage.getItem("photoHistory")) {
+      const history = JSON.parse(localStorage.getItem("photoHistory"));
+      return history;
+    } else return [];
+};
---
+++ part6_browsersAPI/hw3_Unsplash/js/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import { loadNextPhoto, loadRandomPhoto, changeHistory, main } from "./fns.js";
+
+//const photos =  loadNextPhoto();
+const photos =  loadRandomPhoto();
+
+
+
+    main.addEventListener('click', function (e) {
+        const icon = e.target.classList.contains('click');
+        let likes =document.querySelector('.count-likes');
+        const url = document.querySelector('.img').src;
+       
+        if (icon) {
+          const count = +likes.textContent.split(': ')[1] + 1;
+          likes.innerHTML = likes.textContent.split(': ')[0] + ': ' + count;
+          changeHistory(url, count);
+          document.querySelector('.icon').classList.remove('click');
+        };
+      });
+  
